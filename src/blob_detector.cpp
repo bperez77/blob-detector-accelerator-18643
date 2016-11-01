7a2cb90be329d29da9f946b8eb0823f21c11a3c1
Brandon Perez <bmperez@andrew.cmu.edu>
2016-11-01 16:45:56 -0400

    Added in a very simple skeleton for the blob detector application.

---
+++ src/blob_detector.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file blob_detector.cpp
+ * @date Tuesday, November 01, 2016 at 04:44:05 PM EDT
+ * @author Brandon Perez (bmperez)
+ * @author Devon White (dww)
+ * @author Yiyi Zhang (yiyiz)
+ *
+ * @bug No known bugs.
+ **/
+
+// TODO: Fill with the blob detection app that DMA's the image.
+
+int main()
+{
+    return 0;
+}
