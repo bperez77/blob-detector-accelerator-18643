f1d7b3f28a036b783b171ca15b4c73d8c8dffb04
Devon W. White <dww@horizon.andrew.cmu.edu>
2016-11-11 16:40:27 -0500

    Fixed the window fetch kernel. Now runs the full summing kernel.

--- hardware/include/windowfetch.h
+++ hardware/include/windowfetch.h
@@ -20,29 +20,32 @@
 //IN_STREAM_T should be a wrapper of IN_T 
 //OUT_STREAM_T should be a wrapper of OUT_T
 template <typename IN_T, typename OUT_T, typename IN_STREAM_T, typename OUT_STREAM_T, 
-          int IMAGE_WIDTH, int KERNEL_HEIGHT, int KERNEL_WIDTH>
+          int IMAGE_WIDTH, int KERNEL_HEIGHT, int KERNEL_WIDTH,
+          OUT_T (*window_f)(IN_T window[KERNEL_HEIGHT][KERNEL_WIDTH])>
 struct window_pipeline {
     
     IN_T rowbuffer[KERNEL_HEIGHT][IMAGE_WIDTH];
     IN_T window[KERNEL_HEIGHT][KERNEL_WIDTH];
 
-    typedef OUT_T (*window_f)(IN_T window[KERNEL_HEIGHT][KERNEL_WIDTH]);
-    const window_f window_function;
-    //rowbuffer idx
-    int head_row;
-    int tail_row; //The row we store into 
-    int tail_col; //The col we store into -- equal to head_col
+    //typedef OUT_T (*window_f)(IN_T window[KERNEL_HEIGHT][KERNEL_WIDTH]);
+    //const window_f window_function;
+    //rowbuffer idx
+    uint head_row;
+    uint tail_row; //The row we store into
+    uint tail_col; //The col we store into -- equal to head_col
+    uint init_row; //Are we doing the first row for the first time
 
     //window idx 
-    int head_win;
-    int tail_win;
+    uint head_win;
+    uint tail_win;
 
     // Constructor
-    window_pipeline(window_f window_function) : window_function(window_function) {
+    window_pipeline() {
         //initialize head and tail row
         head_row = 0;
         tail_row = 0;
         tail_col = 0; //Equal to head_col
+        init_row = 1;
 
         head_win = 0;
         tail_win = 0;
@@ -53,7 +56,6 @@ struct window_pipeline {
     { 
         IN_T in_pkt;
         OUT_T out_pkt;
-
         //Input Processing 
         in_stream >> in_pkt;
         // tail is the newest elem
@@ -62,25 +64,31 @@ struct window_pipeline {
         
 
         //Window Forming
-        if ((head_row - tail_row) % KERNEL_HEIGHT == 1) {
+        printf("Row, head: %d, tail: %d, diff: %d\n", head_row, tail_row, diffOne(head_row, tail_row, KERNEL_HEIGHT));
+        if (diffOne(head_row, tail_row, KERNEL_HEIGHT)) {
             for (int i = 0; i < KERNEL_HEIGHT; i++) {
+            	printf("Store window row: %d, col: %d, data: %d\n", i, tail_win, rowbuffer[i][tail_col].tdata);
                 window[i][tail_win] = rowbuffer[i][tail_col]; //When i == tail_col we should really be forwarding, not loading again
             }
+            printf("Window, head: %d, tail: %d, diff: %d\n", head_win, tail_win, diffOne(head_win, tail_win, KERNEL_WIDTH));
+            //Output Processing
+            if (diffOne(head_win, tail_win, KERNEL_WIDTH)) {
+                out_pkt = window_f(window);
+                out_stream << out_pkt;
+            }
             tail_win = (tail_win + 1) % KERNEL_WIDTH;
+            head_win = (tail_win == head_win) ? head_win + 1 : head_win;
         }
 
-
+        //printf("Window, head:%d, tail:%d, mod:%d\n", head_win, tail_win, diffOne(head_win, tail_win));
         //Output Processing
-        if ((head_win - tail_win) % KERNEL_WIDTH == 1) {
-            out_pkt = window_function(window);
-            out_stream << out_pkt;
-        }
+
 
         //update the head, tail position 
         update_tail();
         update_head();
 
-        }
+     }
      
 
 
@@ -88,7 +96,8 @@ struct window_pipeline {
      void update_tail(){ 
         if ((tail_col + 1) == IMAGE_WIDTH){
              //update row
-             tail_row = (tail_row + 1) % KERNEL_WIDTH;
+             tail_row = (tail_row + 1) % KERNEL_HEIGHT;
+             init_row = 0;
         }
         tail_col = (tail_col + 1) % IMAGE_WIDTH;
      }
@@ -96,9 +105,15 @@ struct window_pipeline {
      // update head 
      void update_head(){
          //update head_row
-         if (tail_row == head_row){
-            head_row = (head_row + 1) % KERNEL_WIDTH;
+         if (tail_row == head_row && !init_row){
+            head_row = (head_row + 1) % KERNEL_HEIGHT;
          }
+     }
+
+     int diffOne(uint h, uint t, uint size) { //(h - t) % KERNEL_WIDTH == 1
+    	 if ((h - t) == 1) return 1;
+    	 if (h == 0 && (t == size - 1)) return 1;
+    	 return 0;
      }
 
      
--- hardware/lib/windowfetch_test.cpp
+++ hardware/lib/windowfetch_test.cpp
@@ -18,18 +18,21 @@ typedef axis<int, 32> num_axis_t;
 typedef hls::stream<num_axis_t> num_stream_t;
 const int KERNEL_HEIGHT = 4;
 const int KERNEL_WIDTH = 3;
+const int IMAGE_WIDTH = 32; //Factor of Kheight
 
 
 //this is just a simple summing kernel
 num_axis_t sum_window(num_axis_t window[KERNEL_HEIGHT][KERNEL_WIDTH]){
     int sum, last;
     num_axis_t pkt, outpkt;
+    printf("Sum Window\n");
     last = 0;
     for(int i = 0; i < KERNEL_HEIGHT; i++){
         for(int j = 0; j < KERNEL_WIDTH; j++){
-            pkt = window[i][j];
+            pkt = window[i][j];//(j+head_col) % KERNEL_WIDTH];
             sum += pkt.tdata;
-            last = (last || pkt.tlast) ? 1 : 0; 
+            printf("Window, i:%d, j:%d, data: %d tlast: %d\n", i, j, pkt.tdata, pkt.tlast.to_int());
+            if (pkt.tlast.to_int()) last = 1;
         }
     }
     outpkt.tdata = sum;
@@ -43,7 +46,7 @@ int main() {
     //num_axis_t -> IN_T num_axis_t -> OUT_T
     //num_stream_t -> IN_STREAM_T num_stream_t-> OUT_STREAM_T
     //32 -> IMAGE_WIDTH 3-> KERNEL_HEIGHT 4-> KERNEL_WIDTH
-    window_pipeline<num_axis_t, num_axis_t, num_stream_t, num_stream_t, 32, 4, 3> w(sum_window);
+    window_pipeline<num_axis_t, num_axis_t, num_stream_t, num_stream_t, IMAGE_WIDTH, KERNEL_HEIGHT, KERNEL_WIDTH, sum_window> w;
 
     num_stream_t stream1;
     num_stream_t stream2;
@@ -52,16 +55,19 @@ int main() {
         inpkt.tdata = i;
         inpkt.tkeep = -1;
         inpkt.tlast = (i == 1023) ? 1 : 0;
+        //printf("Tlast: %d\n", inpkt.tlast.to_int());
         stream1 << inpkt;
     }
     int last = 0;
     while (last == 0) {
+    	printf("Window Op\n");
         w.window_op(stream1, stream2);
-        int load_success;
-        num_axis_t outpkt;
-        stream2 >> outpkt; //Assume returns 0 if success
-        last = outpkt.tlast.to_int();
-        printf("Sum = %d", outpkt.tdata);
+        if (!stream2.empty()) {
+        	num_axis_t outpkt;
+        	stream2 >> outpkt;
+        	last = outpkt.tlast.to_int();
+        	printf("Sum = %d, last = %d\n", outpkt.tdata, last);
+        }
     }
 
     return 0;
