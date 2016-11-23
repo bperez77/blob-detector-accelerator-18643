721150b4fbdbfbc3f77792675602472af0a4d3bb
Brandon Perez <bmperez@andrew.cmu.edu>
2016-11-23 15:59:31 -0500

    Added in the interface and skeleton code for the blob detector.
    This is responsible for detecting blobs at a single scale in the image.
    Also, the parameters for the algorithm (e.g. threshold value) were added
    as well.

---
+++ hardware/blob_detection/blob_detection.cpp
@@ -0,0 +1,109 @@
+/**
+ * @file blob_detection.cpp
+ * @date Wednesday, November 23, 2016 at 02:29:28 PM EST
+ * @author Brandon Perez (bmperez)
+ * @author Devon White (dww)
+ * @author Yiyi Zhang (yiyiz)
+ *
+ * This file contains the implementation of the blob detection module.
+ *
+ * The blob detection module detects blobs at a single scale in the image, and
+ * is used by the multi-scale blob detector to perform the detection at multiple
+ * scales. Blobs are detected using an LoG filter, with the filter specifically
+ * tuned for circular or elliptical light blobs in the image. These correspond
+ * to potential headlights in the image.
+ *
+ * @bug No known bugs.
+ **/
+
+#include <ap_fixed.h>               // Arbitrary precision fixed-point types
+
+#include "axis.h"                   // Definition of the AXIS protocol structure
+#include "monochrome.h"             // Definition of the monochrome types
+#include "blob_detection.h"         // Our interface and blob detection types
+
+/*----------------------------------------------------------------------------
+ * Internal Definitions
+ *----------------------------------------------------------------------------*/
+
+/**
+ * The fractional precision used for the LoG computation. This determines how
+ * many bits are used for the fractional part of the fixed point representation.
+ **/
+static const int LOG_FRACTIONAL_BITS = 7;
+
+/**
+ * The type of an LoG response. This is a fixed point value with 1 bit for the
+ * integer part, and LOG_FRACTIONAL_BITS bits for the fractional part. As all
+ * the monochrome values are either 0 or 1, we only need 1 integral bit.
+ **/
+typedef ap_fixed<LOG_FRACTIONAL_BITS+1, LOG_FRACTIONAL_BITS> log_response_t;
+
+/**
+ * The threshold value used to determine if an LoG response corresponds to a
+ * blob detection. If the response is greater than or equal to the threshold,
+ * it becomes 1, otherwise, it becomes 0.
+ **/
+static const log_response_t LOG_RESPONSE_THRESHOLD = 0.492 * 1.0;
+
+/**
+ * The LoG filter kernel used to determine the LoG response for a window of the
+ * image.
+ **/
+static const log_response_t LOG_FILTER[BLOB_FILTER_HEIGHT][BLOB_FILTER_WIDTH] = {
+    {-0.0239, -0.0460, -0.0499, -0.0460, -0.0239},
+    {-0.0460, -0.0061,  0.0923, -0.0061, -0.0460},
+    {-0.0499,  0.0923,  0.3182,  0.0923, -0.0499},
+    {-0.0460, -0.0061,  0.0923, -0.0061, -0.0460},
+    {-0.0239, -0.0460, -0.0499, -0.0460, -0.0239},
+};
+
+/*----------------------------------------------------------------------------
+ * LoG Filter Module
+ *----------------------------------------------------------------------------*/
+
+/**
+ * Decides if the given window corresponds to a blob detection.
+ *
+ * This is the combinational interface to the module.
+ **/
+blob_detection_t compute_blob_detection(monochrome_window_t window) {
+#pragma HLS INLINE
+
+    // FIXME: Implement
+    return 0;
+}
+
+/**
+ * Converts the monochrome stream into a stream of blob detections.
+ *
+ * The blob detections are binary values that indicate if the pixel is the
+ * centerpoint of a detected blob. This is the sequential interface to the
+ * module.
+ **/
+void blob_detection(monochrome_stream_t& monochrome_stream,
+        blob_detection_stream_t& blob_detection_stream) {
+#pragma HLS INLINE
+
+    // FIXME: Implement
+    return;
+}
+
+/*----------------------------------------------------------------------------
+ * Top Function for Synthesis
+ *----------------------------------------------------------------------------*/
+
+/**
+ * The top function for the blob detector when it is synthesized by itself.
+ *
+ * This is the function that HLS will look for if the blob detector is
+ * synthesized into its own IP block.
+ **/
+void blob_detection_top(monochrome_stream_t& monochrome_stream,
+        blob_detection_stream_t& blob_detection_stream) {
+#pragma HLS INTERFACE axis port=monochrome_stream
+#pragma HLS INTERFACE axis port=blob_detection_stream
+
+    blob_detection(monochrome_stream, blob_detection_stream);
+    return;
+}
---
+++ hardware/include/blob_detection.h
@@ -0,0 +1,82 @@
+/**
+ * @file blob_detection.h
+ * @date Wednesday, November 23, 2016 at 02:59:28 PM EST
+ * @author Brandon Perez (bmperez)
+ * @author Devon White (dww)
+ * @author Yiyi Zhang (yiyiz)
+ *
+ * This file contains the interface to the blob detectioion module.
+ *
+ * This defines the blob detection module interface, as both a sequential
+ * (streaming) interface, and a combinational interface. It also defines the
+ * input and outptut types for the module.
+ *
+ * @bug No known bugs.
+ **/
+
+#ifndef BLOB_DETECTION_H_
+#define BLOB_DETECTION_H_
+
+#include <hls_stream.h>             // Definition of the hls::stream class
+#include <ap_int.h>                 // Arbitrary precision integer types
+
+#include "axis.h"                   // Definition of the AXIS protocol structure
+#include "monochrome.h"             // Definition of the monochrome types
+
+/*----------------------------------------------------------------------------
+ * Definitions
+ *----------------------------------------------------------------------------*/
+
+/**
+ * The dimensions of the blob filter (which is LoG). This also determines the
+ * size of the window operated on in the image.
+ **/
+static const int BLOB_FILTER_WIDTH = 5;
+static const int BLOB_FILTER_HEIGHT = BLOB_FILTER_WIDTH;
+
+/**
+ * A convenient alias for the window of monochrome values matching the filter
+ * size.
+ **/
+typedef monochrome_t monochrome_window_t[BLOB_FILTER_HEIGHT][BLOB_FILTER_WIDTH];
+
+/**
+ * The input stream type is a monochrome axis packet. The output stream type is
+ * a 1-bit boolean value axis packet. This boolean indicates if the pixel
+ * corresponds to the centerpoint of a blob detection.
+ **/
+typedef ap_uint<1> blob_detection_t;
+typedef axis<blob_detection_t, 1> blob_detection_axis_t;
+typedef hls::stream<blob_detection_axis_t> blob_detection_stream_t;
+
+/*----------------------------------------------------------------------------
+ * Interface
+ *----------------------------------------------------------------------------*/
+
+/**
+ * Decides if the given window in the image corresponds a blob detection.
+ *
+ * This computes the LoG filter response for the given window of monochrome
+ * values, and thresholds the response to determine if this window corresponds
+ * to a blob. This is the combinational interface to the module.
+ *
+ * @param[in] window A window of monochrome values from an image.
+ * @return 1 if the window corresponds to a blob, 0 otherwise.
+ **/
+blob_detection_t compute_blob_detection(monochrome_window_t window);
+
+/**
+ * Converts the monochrome input stream into an output LoG detection stream.
+ *
+ * This is done by computing the LoG response on windows of the image, and
+ * thresholding to determine if it is a blob. The output stream indicates if the
+ * pixel is a centerpoint of a detected blob in an image. This is the sequential
+ * interface to the module.
+ *
+ * @param[in] monochrome_stream The input stream of monochrome values.
+ * @param[out] blob_detection_stream The output stream of LoG detections.
+ **/
+void blob_detection(monochrome_stream_t& monochrome_stream,
+        blob_detection_stream_t& blob_detection_stream);
+
+#endif /* BLOB_DETECTION_H_ */
