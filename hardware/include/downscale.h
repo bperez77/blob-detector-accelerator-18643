3ba054a42c56a9497805fa28b842f08916713d08
Yiyi Zhang <yiyiz.cmu.1129@gmail.com>
2016-12-02 00:49:52 -0500

    latency 8294404 and cycle/pixel = 4

--- hardware/include/downscale.h
+++ hardware/include/downscale.h
@@ -24,7 +24,7 @@
  * The amount that the image is scaled down by at each scale level, in each
  * dimension. For example, a WxH would become(W/FACTOR)x(H/FACTOR) image.
  **/
-static const int DOWNSCALE_FACTOR = 4;
+static const int DOWNSCALE_FACTOR = 5;
 
 /**
  * A convenient alias for the window of grayscale values matching the size of
@@ -66,25 +66,32 @@ grayscale_t compute_downscale(grayscale_window_t window);
  **/
 template <int IMAGE_HEIGHT, int IMAGE_WIDTH>
 void downscale(grayscale_stream_t& grayscale_stream, grayscale_stream_t& downscale_stream) {
+
 #pragma HLS INLINE
 
-    // TODO: Add in fractional support
+	// Add in fractional support
     // Read in all pixels at once
 	grayscale_t grayscale_buffer[DOWNSCALE_FACTOR-1][IMAGE_WIDTH];
-	grayscale_t block[DOWNSCALE_FACTOR][DOWNSCALE_FACTOR];
+#pragma HLS ARRAY_PARTITION variable=grayscale_buffer complete dim=1
+
+
 	grayscale_axis_t downscale_stream_pkt;
+	int b = 0;
 	for(int i = 0; i < IMAGE_HEIGHT; i++){
+		grayscale_t block[DOWNSCALE_FACTOR][DOWNSCALE_FACTOR];
+#pragma HLS ARRAY_PARTITION variable=block complete dim=1
+		int c = 0;
 		for(int j = 0; j < IMAGE_WIDTH; j++){
 		    // buffer the value until the last row of a block height
 		    // iterate through grayscale_buffer
-		    int b = i % DOWNSCALE_FACTOR;
-		    int c = j % DOWNSCALE_FACTOR;
-		    if(i % DOWNSCALE_FACTOR < (DOWNSCALE_FACTOR-1)){
+#pragma HLS PIPELINE II=1
+
+			if( b < (DOWNSCALE_FACTOR-1)){
 		    	grayscale_buffer[b][j] = grayscale_stream.read().tdata;
 		    }
 		    else{
-		       	        block[b][c]= grayscale_stream.read().tdata;
-		       		if (j % DOWNSCALE_FACTOR == DOWNSCALE_FACTOR-1){
+		       	    block[b][c]= grayscale_stream.read().tdata;
+		       		if ( c == DOWNSCALE_FACTOR-1){
 		       			for(int p = 0; p <	(DOWNSCALE_FACTOR - 1); p++){
 		       				for(int q = 0; q < DOWNSCALE_FACTOR; q++){
 		       					block[p][q] = grayscale_buffer[p][j];
@@ -94,13 +101,15 @@ void downscale(grayscale_stream_t& grayscale_stream, grayscale_stream_t& downsca
 		       			// Our transfers are always aligned, so set tkeep to -1, and assert
 		       			// tlast when we reach the last packet
 		       			downscale_stream_pkt.tkeep = -1;
-		       			downscale_stream_pkt.tlast = block[DOWNSCALE_FACTOR-1][DOWNSCALE_FACTOR-1];
-
+		       			downscale_stream_pkt.tlast = (i == IMAGE_HEIGHT-1) &&( j == IMAGE_WIDTH-1);
 		       			// Stream out the grayscale packet
 		       			downscale_stream.write(downscale_stream_pkt);
 		       	    }
 		       	}
+
+			c = (c==DOWNSCALE_FACTOR-1)? 0: (c+1);
 		  }
+		b = (b==DOWNSCALE_FACTOR-1)? 0: (b+1);
 	}
 
     return;
