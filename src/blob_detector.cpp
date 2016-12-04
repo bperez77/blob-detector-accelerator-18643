13ed2905ceb2b0978883c1855b25b3eff65037fc
Brandon Perez <bmperez@andrew.cmu.edu>
2016-12-04 00:07:08 -0500

    Changed the application to read multiple files from a directory.
    The application now processes images from the 'images' directory, and
    saves them under the same name in 'outputs' directory. Also, the
    application was generally cleaned up, and print statement control was
    added.

--- src/blob_detector.cpp
+++ src/blob_detector.cpp
@@ -8,53 +8,72 @@
  * @bug No known bugs.
  **/
 
-#include <cstdlib>              // C standard library
-#include <cstdio>               // C standard I/O library
-#include <cassert>              // Assert macro
+#include <cstdlib>                  // C standard library
+#include <cstdio>                   // C standard I/O library
+#include <cassert>                  // Assert macro
 
-#include <xil_cache.h>          // Cache control functions
-#include <xparameters.h>        // Auto-generated params for FPGA IP
-#include <xaxidma.h>            // Functions and definitions for AXI CDMA
-#include <xtime_l.h>            // Cycle counter timer
-#include <ff.h>                 // Xilinx FAT filesystem interface
+#include <xil_cache.h>              // Cache control functions
+#include <xparameters.h>            // Auto-generated params for FPGA IP
+#include <xaxidma.h>                // Functions and definitions for AXI CDMA
+#include <xtime_l.h>                // Cycle counter timer
+#include <ff.h>                     // Xilinx FAT filesystem interface
+#include <ffconf.h>                 // Xilinx FAT filesystem configuration
 
-#include "image.h"              // Image definitions and the image type
+#include "image.h"                  // Image definitions and the image type
 
 /*----------------------------------------------------------------------------
  * Internal Definitions
  *----------------------------------------------------------------------------*/
 
+// Controls whether or not verbose print statements are enabled
+#define VERBOSE
+
 // A macro to print an error message
 #define log_err(msg, ...) printf("%s: %s: %d: Error: " msg, __FILE__, \
         __func__, __LINE__, ##__VA_ARGS__)
+
+// A macro to enable print messages when DEBUG is defined
+#ifdef VERBOSE
+#define log_verbose(...) printf(__VA_ARGS__)
+#else
+#define log_verbose(...)
+#endif
 
 // Alias for an image containing 32-bit RGBA pixels
 typedef matrix<pixel, IMAGE_WIDTH, IMAGE_HEIGHT> input_image_t;
 
 // A structure representing an AXI DMA device, holds information about the IP
 typedef struct axidma {
-    int id;                     // ID used to identify the device
-    XAxiDma_Config *config;     // Config parameters for the AXI DMA
-    XAxiDma dev;                // CDMA device structure
+    int id;                         // ID used to identify the device
+    XAxiDma_Config *config;         // Config parameters for the AXI DMA
+    XAxiDma dev;                    // AXI DMA device structure
 } axidma_t;
 
 // A structure representing the context on the device
 typedef struct device_context {
-    FATFS sd_card_fs;           // Handle the the SD card filesystem (FAT)
-    axidma_t axidma;            // The AXI DMA device
+    FATFS sd_card_fs;               // Handle the the SD card filesystem (FAT)
+    axidma_t axidma;                // The AXI DMA device
 } devices_context_t;
 
+// Maximum size any given path is allowed to be
+static const size_t MAX_PATH_LEN    = 100;
+
 // Shorten the clunky name for id defines for the AXI DMA devices
-static const int AXIDMA_ID = XPAR_IMAGE_DMA_DEVICE_ID;
+static const int AXIDMA_ID          = XPAR_INPUT_OUTPUT_DMA_DEVICE_ID;
 
 // The path to the SD card for the f_mount function
-static const TCHAR *SD_CARD_PATH = "0:/";
+static const TCHAR *SD_CARD_PATH    = "0:/";
 
-/* The name of the image files. IMPORTANT: For any given file path, no component
- * (file/directory) of the path can exceed 8 characters (including the
- * extension). Also, the extension cannot exceed 3 characters. */
-static const TCHAR *IMAGE_PATH = "im.rgb";
-static const TCHAR *OUTPUT_IMAGE_PATH = "out.rgb";
+/* The name of the input and output directories for the image files. IMPORTANT:
+ * For any given file path, no component (file/directory) of the path can
+ * exceed 8 characters (including the extension). Also, the extension cannot
+ * exceed 3 characters. */
+static const TCHAR *IMAGE_DIR_PATH  = "images";
+static const TCHAR *OUTPUT_DIR_PATH = "output";
+
+// Allocate global buffers for the input and output images
+static image_t IMAGE;
+static image_t OUTPUT_IMAGE;
 
 /*----------------------------------------------------------------------------
  * Initialization
@@ -78,10 +97,10 @@ static int init_axidma(axidma_t& axidma, int device_id)
     }
 
     // Disable all DMA interrupts for both channels, as we're polling
-	XAxiDma_IntrDisable(&axidma.dev, XAXIDMA_IRQ_ALL_MASK,
-			XAXIDMA_DEVICE_TO_DMA);
-	XAxiDma_IntrDisable(&axidma.dev, XAXIDMA_IRQ_ALL_MASK,
-						XAXIDMA_DMA_TO_DEVICE);
+    XAxiDma_IntrDisable(&axidma.dev, XAXIDMA_IRQ_ALL_MASK,
+            XAXIDMA_DEVICE_TO_DMA);
+    XAxiDma_IntrDisable(&axidma.dev, XAXIDMA_IRQ_ALL_MASK,
+                        XAXIDMA_DMA_TO_DEVICE);
 
     return XST_SUCCESS;
 }
@@ -109,18 +128,38 @@ static int init_devices(devices_context_t& devices)
  * File I/O Handling
  *----------------------------------------------------------------------------*/
 
-static int open_image(const TCHAR *image_path, image_t& image)
+static void join_paths(const char* path1, const char *path2, char *output_path,
+        size_t output_size)
 {
+    assert(strlen(path1) + 1 + strlen(path2) <= output_size - 1);
+
+    // Copy path one to the output
+    size_t path1_len = strlen(path1);
+    strncpy(output_path, path1, output_size);
+
+    // Join path2 with a '/' character and copy path2
+    output_path[path1_len] = '/';
+    strncpy(output_path + path1_len + 1, path2, output_size - path1_len - 1);
+    return;
+}
+
+static int open_image(const TCHAR *root_path, const TCHAR *tail_path,
+        image_t& image)
+{
+    // Join the root and tail paths to get the path to the image
+    TCHAR image_path[MAX_PATH_LEN+1];
+    join_paths(root_path, tail_path, image_path, sizeof(image_path));
+
     // Try to open the specified image file
     FIL file;
-    int rc = f_open(&file, image_path, FA_READ);
+    FRESULT rc = f_open(&file, image_path, FA_READ);
     if (rc != FR_OK) {
         log_err("%s: Unable to open input image file.\n", image_path);
         return rc;
     }
 
     // Check that the file is the expected size
-    if (file.fsize != image.size()) {
+    if (file_size(&file) != image.size()) {
         log_err("%s: File size does not match input image's. Expected %u "
                 "bytes, but the file size is %lu.\n", image_path, image.size(),
                 file.fsize);
@@ -146,11 +185,16 @@ static int open_image(const TCHAR *image_path, image_t& image)
     return XST_SUCCESS;
 }
 
-static int save_image(const TCHAR *image_path, const image_t& image)
+static int save_image(const TCHAR *root_path, const TCHAR *tail_path,
+        const image_t& image)
 {
+    // Join the root and tail paths to get the path to the image
+    TCHAR image_path[MAX_PATH_LEN+1];
+    join_paths(root_path, tail_path, image_path, sizeof(image_path));
+
     // Try to open the specified output file
     FIL file;
-    int rc = f_open(&file, image_path, FA_CREATE_ALWAYS|FA_WRITE);
+    FRESULT rc = f_open(&file, image_path, FA_CREATE_ALWAYS|FA_WRITE);
     if (rc != FR_OK) {
         log_err("%s: Unable to open output image file.\n", image_path);
         return rc;
@@ -183,38 +227,93 @@ static int save_image(const TCHAR *image_path, const image_t& image)
  * Main Application
  *----------------------------------------------------------------------------*/
 
-// Declare the image, allocating room for its buffer (stack is too small)
-static image_t TEST_IMAGE;
-static image_t OUTPUT_IMAGE;
-
 static int send_image(axidma_t& axidma, const image_t& image,
-		image_t& output_image)
+        image_t& output_image)
 {
-	// Get a handle to the AXI DMA device
-	XAxiDma* dma_dev = &axidma.dev;
+    // Get a handle to the AXI DMA device
+    XAxiDma* dma_dev = &axidma.dev;
 
-	// Initiate the transfer to send the image to the FPGA
-	int rc = XAxiDma_SimpleTransfer(dma_dev, (u32)image.buffer, image.size(),
-			XAXIDMA_DMA_TO_DEVICE);
-	if (rc < 0) {
-		log_err("Unable to start image transfer over AXI DMA to the FPGA.\n");
-		return rc;
-	}
+    // Initiate the transfer to receive the output image from the FPGA
+    int rc = XAxiDma_SimpleTransfer(dma_dev, (u32)output_image.buffer,
+            output_image.size(), XAXIDMA_DEVICE_TO_DMA);
+    if (rc != XST_SUCCESS) {
+        log_err("Unable to start image transfer over AXI DMA from the FPGA.\n");
+        return rc;
+    }
 
-	// Initiate the transfer to receive the output image from the FPGA
-	rc = XAxiDma_SimpleTransfer(dma_dev, (u32)output_image.buffer,
-			output_image.size(), XAXIDMA_DEVICE_TO_DMA);
-	if (rc < 0) {
-		log_err("Unable to start image transfer over AXI DMA from the FPGA.\n");
-		return rc;
-	}
+    // Initiate the transfer to send the image to the FPGA
+    rc = XAxiDma_SimpleTransfer(dma_dev, (u32)image.buffer, image.size(),
+            XAXIDMA_DMA_TO_DEVICE);
+    if (rc != XST_SUCCESS) {
+        log_err("Unable to start image transfer over AXI DMA to the FPGA.\n");
+        return rc;
+    }
 
-	/* Wait for both the transfers to complete. We really only need to wait on the
-	 * receive transfer, as it depends on the transmit transfer. */
-	while (XAxiDma_Busy(dma_dev, XAXIDMA_DMA_TO_DEVICE) &&
-			XAxiDma_Busy(dma_dev, XAXIDMA_DEVICE_TO_DMA));
+    /* Wait for both the transfers to complete. We really only need to wait on the
+     * receive transfer, as it depends on the transmit transfer. */
+    while (XAxiDma_Busy(dma_dev, XAXIDMA_DMA_TO_DEVICE) &&
+            XAxiDma_Busy(dma_dev, XAXIDMA_DEVICE_TO_DMA));
 
-	return 0;
+    return XST_SUCCESS;
+}
+
+static int blob_detection(axidma_t axidma, const TCHAR *image_dir,
+        const TCHAR *image_name, const TCHAR *output_dir)
+{
+    // Get handles to the global buffers for the images
+    image_t& image = IMAGE;
+    image_t& output_image = OUTPUT_IMAGE;
+
+    // Open the image file, and load it into memory
+    log_verbose("\tOpening the image file in '%s'...\n", image_dir);
+    int rc = open_image(image_dir, image_name, image);
+    if (rc != XST_SUCCESS) {
+        return rc;
+    }
+
+    /* Use the hardware on the FPGA to perform an operation on the image,
+     * sending it out, and receiving a new output image. */
+    log_verbose("\tTransferring the image over the fabric...\n");
+    rc = send_image(axidma, image, output_image);
+    if (rc != XST_SUCCESS) {
+        return rc;
+    }
+
+    // Save the image file
+    log_verbose("\tSaving the image to file '%s'...\n", output_dir);
+    rc = save_image(output_dir, image_name, output_image);
+    if (rc != XST_SUCCESS) {
+        return rc;
+    }
+
+    return XST_SUCCESS;
+}
+
+static int run_blob_detections(axidma_t& axidma, const TCHAR* image_dir_path,
+        DIR *image_dir, const TCHAR *output_dir_path)
+{
+    // Read and process each image file in the directory
+    while (true) {
+        // Get the next file in the input image directory, stop when none left
+        FILINFO file_info;
+        FRESULT f_rc = f_readdir(image_dir, &file_info);
+        if (f_rc != FR_OK) {
+            log_err("%s: Unable to read the next file in the input image "
+                    "directory.\n", image_dir_path);
+            return f_rc;
+        } else if (strlen(file_info.fname) == 0) {
+            return XST_SUCCESS;
+        }
+
+        // Run blob detection on this image, and save it to its output file
+        printf("\nRunning blob detection on file '%s' in '%s'...\n",
+                file_info.fname, image_dir_path);
+        int rc = blob_detection(axidma, image_dir_path, file_info.fname,
+                output_dir_path);
+        if (rc != XST_SUCCESS) {
+            return rc;
+        }
+    }
 }
 
 int main()
@@ -223,37 +322,37 @@ int main()
     printf("-------------------------\n");
 
     // Initialize the system, namely the SD card and AXI DMA
-    printf("\tMounting the SD card and initializing AXI DMA devices...\n");
+    printf("Mounting the SD card and initializing AXI DMA devices...\n");
     devices_context_t devices_context;
     int rc = init_devices(devices_context);
     if (rc != XST_SUCCESS) {
         return rc;
     }
 
-    // Open the image file, and load it into memory
-    printf("\tOpening the image file '%s'...\n", IMAGE_PATH);
-    image_t& image = TEST_IMAGE;
-    rc = open_image(IMAGE_PATH, image);
+    // Open the input directory containing the images
+    FRESULT f_rc;
+    DIR input_dir;
+    f_rc = f_opendir(&input_dir, IMAGE_DIR_PATH);
+    if (f_rc != FR_OK) {
+        log_err("%s: Unable to open input image directory.\n", IMAGE_DIR_PATH);
+        return f_rc;
+    }
+
+    // Create the output directory if it doesn't already exist
+    f_rc = f_mkdir(OUTPUT_DIR_PATH);
+    if (f_rc != FR_OK && f_rc != FR_EXIST) {
+        log_err("%s: Unable to create output directory.\n", OUTPUT_DIR_PATH);
+        return rc;
+    }
+
+    // Run blob detection on all the images in the input directory
+    rc = run_blob_detections(devices_context.axidma, IMAGE_DIR_PATH, &input_dir,
+            OUTPUT_DIR_PATH);
     if (rc != XST_SUCCESS) {
         return rc;
     }
 
-    /* Use the hardware on the FPGA to perfrom an operation on the image,
-     * sending it out, and receiving a new output image. */
-    printf("\tTransferring the image over the fabric...\n");
-    image_t& output_image = OUTPUT_IMAGE;
-    rc = send_image(devices_context.axidma, image, output_image);
-    if (rc < 0) {
-    	return rc;
-    }
-
-    // Save the image file
-    printf("\tSaving the image to file '%s'...\n", OUTPUT_IMAGE_PATH);
-    rc = save_image(OUTPUT_IMAGE_PATH, output_image);
-    if (rc < 0) {
-        return rc;
-    }
-
-    printf("Success\n");
-    return 0;
+    printf("\nBlob detection is complete. Output images can be found in %s.\n",
+            OUTPUT_DIR_PATH);
+    return XST_SUCCESS;
 }
